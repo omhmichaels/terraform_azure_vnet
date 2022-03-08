91938c33a07feae2554be27680bfa8ba4167ac29
Nathaniel Davila <nat.admin@inventati.org>
2022-03-08 12:56:46 -0800

    fix typo

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,6 @@ output "vnet_name" {
   value = azurerm_virtual_network.vnet.name
 }
 
-output "vnet_resource_groupt" {
+output "vnet_resource_group" {
   value = azurerm_virtual_network.vnet.resource_group_name
 }
